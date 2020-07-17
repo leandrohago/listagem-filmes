6076fe16802e199889de776ff92bc99c71bc34ed
Leandro H Agostinho <leandro.henrique@rgb.com.br>
2020-07-16 22:30:16 -0300

    array de gêneros no select

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -10,6 +10,7 @@ import { ValidarCamposService } from 'src/app/shared/components/campos/validar-c
 export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
+  generos: Array<string>
 
   constructor(public validacao: ValidarCamposService, private fb: FormBuilder) { }
 
@@ -27,6 +28,9 @@ export class CadastroFilmesComponent implements OnInit {
       ,urlIMDb: ['', Validators.minLength(10)]
       ,genero: ['', Validators.required]
     });
+
+    this.generos = ['Ação', 'Aventura', 'Comédia', 'Ficção Científica', 'Romance', 'Terror']
+
   }
     
   salvar(): void {
